51ea5c1d9974ec77160b5e64a794aeb90f873fdf
Jason Mei <jason.mei403@gmail.com>
2018-06-10 04:02:48 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,8 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {
-          name: "Benvolio",
+        { name: "Benvolio",
           age: "17",
           attitude: "worried"
         },
@@ -22,13 +21,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {
-          name: "Steven",
+        { name: "Steven",
           age: "30"
           attitude: "confused"
         },
-        {
-          name: "Nurse",
+        { name: "Nurse",
           age: "44"
           attitude: "worried"
         }
